0b740c2ba25b15f5e554f1d80ed6c20d64d7d686
Vandhana <vandhana.jayakumar1106@gmail.com>
2024-04-12 10:31:14 +0530

    response in the postman was changed

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,28 +1,90 @@
-# from rest_framework import generics
+
+#1st
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
 # from .models import MachineData
 # from .serializers import MachineDataSerializer
+# import logging
 
-# class MachineDataListCreate(generics.ListCreateAPIView):
-#     queryset = MachineData.objects.all()
-#     serializer_class = MachineDataSerializer
+# logger = logging.getLogger(__name__)
 
-# class MachineDataRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = MachineData.objects.all()
-#     serializer_class = MachineDataSerializer
+# @api_view(['GET', 'POST'])
+# def machine_data_list_create(request):
+#     if request.method == 'GET':
+#         queryset = MachineData.objects.all()
+#         serializer = MachineDataSerializer(queryset, many=True)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = MachineDataSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=201)
+#         logger.error(serializer.errors) 
+#         return Response(serializer.errors, status=400)
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def machine_data_detail(request, pk):
+#     try:
+#         machine_data = MachineData.objects.get(pk=pk)
+#     except MachineData.DoesNotExist:
+#         return Response(status=404)
+
+#     if request.method == 'GET':
+#         serializer = MachineDataSerializer(machine_data)
+#         return Response(serializer.data)
+#     elif request.method == 'PUT':
+#         serializer = MachineDataSerializer(machine_data, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         logger.error(serializer.errors)  
+#         return Response(serializer.errors, status=400)
+#     elif request.method == 'DELETE':
+#         machine_data.delete()
+#         return Response(status=204)
 
 
-# from rest_framework.response import Response
+#2
 # from rest_framework.decorators import api_view
+# from rest_framework.response import Response
 # from .models import MachineData
-# @api_view(['GET'])
-# def getData(request):
-#     machine_data = MachineData.objects.all()
-#     response_data = {
-#         'count': machine_data.count(),
-#         'data': [{'id': entry.id, 'MACHINEID': entry.MACHINEID, 'DATE': entry.DATE} for entry in machine_data]
-#     }
-    
-#     return Response(response_data)
+# from .serializers import MachineDataSerializer
+# import logging
+
+# logger = logging.getLogger(__name__)
+
+# @api_view(['GET', 'POST'])
+# def machine_data_list_create(request):
+#     if request.method == 'GET':
+#         return Response({"message": "Data retrieved successfully"})
+#     elif request.method == 'POST':
+#         serializer = MachineDataSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({"message": "Data created successfully"}, status=201) 
+#         logger.error(serializer.errors) 
+#         return Response(serializer.errors, status=400)
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def machine_data_detail(request, pk):
+#     try:
+#         machine_data = MachineData.objects.get(pk=pk)
+#     except MachineData.DoesNotExist:
+#         return Response(status=404)
+
+#     if request.method == 'GET':
+#         return Response({"message": "Data retrieved successfully"})
+#     elif request.method == 'PUT':
+#         serializer = MachineDataSerializer(machine_data, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         logger.error(serializer.errors)  
+#         return Response(serializer.errors, status=400)
+#     elif request.method == 'DELETE':
+#         machine_data.delete()
+#         return Response(status=204)
+
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -35,15 +97,18 @@ logger = logging.getLogger(__name__)
 @api_view(['GET', 'POST'])
 def machine_data_list_create(request):
     if request.method == 'GET':
-        queryset = MachineData.objects.all()
-        serializer = MachineDataSerializer(queryset, many=True)
-        return Response(serializer.data)
+        if 'Postman' in request.META.get('HTTP_USER_AGENT', ''):
+            return Response({"message": "Data retrieved successfully"})
+        else:
+            queryset = MachineData.objects.all()
+            serializer = MachineDataSerializer(queryset, many=True)
+            return Response(serializer.data)
     elif request.method == 'POST':
         serializer = MachineDataSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
-        logger.error(serializer.errors)  # Log the serializer errors
+            return Response({"message": "Data created successfully"}, status=201) 
+        logger.error(serializer.errors) 
         return Response(serializer.errors, status=400)
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -54,14 +119,17 @@ def machine_data_detail(request, pk):
         return Response(status=404)
 
     if request.method == 'GET':
-        serializer = MachineDataSerializer(machine_data)
-        return Response(serializer.data)
+        if 'Postman' in request.META.get('HTTP_USER_AGENT', ''):
+            return Response({"message": "Data retrieved successfully"})
+        else:
+            serializer = MachineDataSerializer(machine_data)
+            return Response(serializer.data)
     elif request.method == 'PUT':
         serializer = MachineDataSerializer(machine_data, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        logger.error(serializer.errors)  # Log the serializer errors
+        logger.error(serializer.errors)  
         return Response(serializer.errors, status=400)
     elif request.method == 'DELETE':
         machine_data.delete()
